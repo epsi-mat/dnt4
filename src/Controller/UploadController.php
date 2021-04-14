82c6545d685d44b6ce1c6f64a5bf8ba085784306
Yoann <yoann.clement@epsi.fr>
2021-04-15 00:26:15 +0200

    YC - goooooooooooooooooood api

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class UploadController extends AbstractController
 {
@@ -18,28 +17,27 @@ class UploadController extends AbstractController
      */
     public function createFile(Request $request, EntityManagerInterface $em){
         $file = new File();
-        $data_entity = new Data();
         $content_file_implode = array();
 
         $json_file = $request->files->get('file');
         $content_file = file($json_file);
 
         for($row = 0; $row < count($content_file); $row++){
-            $content_file_implode[] = explode(",", $content_file[$row]);
+            $content_file[$row] = str_replace("\r\n", "", $content_file[$row]);
+            $content_file_implode[] = explode(";", $content_file[$row]);
         }
 
+        $file->setName($json_file->getClientOriginalName());
         foreach($content_file_implode as $item){
-
+            $data_entity = new Data();
             $data_entity->setName($item[0]);
             $data_entity->setContent($item[1]);
-
-            $file->setName($json_file->getClientOriginalName());
             $file->addData($data_entity);
-
             $em->persist($file);
             $em->flush();
         }
 
         return $this->json($file, 201, [], ['groups' => 'post:read']);
     }
+
 }
