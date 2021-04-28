bbe41e04fe43745f8514c13dce5bb11050017439
Yoann <yoann.clement@epsi.fr>
2021-04-28 21:32:47 +0200

    Test d'application
    test application route GET

--- tests/Controller/UploadTest.php
+++ tests/Controller/UploadTest.php
@@ -5,22 +5,13 @@ namespace App\Tests\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UploadTest extends WebTestCase
 {
-    public function testUpload() : void
+    public function testGetFile() : void
     {
         $client = static::createClient();
-
-        $file_upload = new UploadedFile(
-            'public/uploads/files/myFile.csv',
-            'myFile.csv',
-            'text/csv'
-        );
-
-        $client->request('POST', '/api/files',[],['file' => $file_upload]);
-
-        $this->assertResponseStatusCodeSame(201);
+        $client->request('GET', '/api/files');
+        $this->assertResponseStatusCodeSame(200);
     }
 }
