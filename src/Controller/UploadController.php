9e68f152c395b995d2e2b49c00a2a1065d451c58
Yoann <yoann.clement@epsi.fr>
2021-04-28 21:29:34 +0200

    Route GET
    Requête get

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -6,9 +6,12 @@ use App\Entity\DataFile;
 use App\Entity\File;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -23,10 +26,13 @@ class UploadController extends AbstractController
     }
 
     /**
-     * @Route("/api/files", name="", methods={"GET"})
+     * @Route("/api/files", name="api_get_file", methods={"GET"})
      */
     public function getFile(){
-
+        $files = $this->getDoctrine()
+            ->getRepository(File::class)
+            ->findAll();
+        return $this->json($files, 200, [],['groups' => 'post:read']);
     }
 
     /**
