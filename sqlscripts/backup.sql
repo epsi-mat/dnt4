a6f2fb9782c6f065a655f48760c43217c85de5f1
mat-planchot <mat_planchot@hotmail.fr>
2021-04-29 15:34:19 +0200

    error backup empty

--- sqlscripts/backup.sql
+++ sqlscripts/backup.sql
@@ -14,6 +14,58 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `data_file`
+--
+
+DROP TABLE IF EXISTS `data_file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `data_file` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `file_id` int DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_37D0FDF293CB796C` (`file_id`),
+  CONSTRAINT `FK_37D0FDF293CB796C` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `data_file`
+--
+
+LOCK TABLES `data_file` WRITE;
+/*!40000 ALTER TABLE `data_file` DISABLE KEYS */;
+INSERT INTO `data_file` VALUES (1,1,'\" l','ha'),(2,1,'af','EC');
+/*!40000 ALTER TABLE `data_file` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `file`
+--
+
+DROP TABLE IF EXISTS `file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `file` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `file`
+--
+
+LOCK TABLES `file` WRITE;
+/*!40000 ALTER TABLE `file` DISABLE KEYS */;
+INSERT INTO `file` VALUES (1,'myFile');
+/*!40000 ALTER TABLE `file` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +76,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-25 21:23:04
+-- Dump completed on 2021-04-24  7:22:44
--- sqlscripts/dev/structure_table.sql
+++ sqlscripts/dev/structure_table.sql
@@ -1,35 +1,31 @@
-DROP TABLE IF EXISTS `data`;
-CREATE TABLE `data` (
-                        `id` int NOT NULL AUTO_INCREMENT,
-                        `file_id` int DEFAULT NULL,
-                        `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                        `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                        PRIMARY KEY (`id`),
-                        KEY `IDX_ADF3F36393CB796C` (`file_id`),
-                        CONSTRAINT `FK_ADF3F36393CB796C` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 --
--- Dumping data for table `data`
+-- Table structure for table `data_file`
 --
 
-LOCK TABLES `data` WRITE;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `data_file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `data_file` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `file_id` int DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_37D0FDF293CB796C` (`file_id`),
+  CONSTRAINT `FK_37D0FDF293CB796C` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `file`
 --
 
 DROP TABLE IF EXISTS `file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `file` (
-                        `id` int NOT NULL AUTO_INCREMENT,
-                        `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-                        PRIMARY KEY (`id`)
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `file`
---
-
-LOCK TABLES `file` WRITE;
-UNLOCK TABLES;
-
+/*!40101 SET character_set_client = @saved_cs_client */;
