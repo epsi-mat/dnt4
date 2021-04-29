f240209aa0cfe14440c23893528281d1e15b5b93
mat-planchot <mat_planchot@hotmail.fr>
2021-04-29 17:48:32 +0200

    revert jenkinsfile

--- sqlscripts/dev/structure_table.sql
+++ sqlscripts/dev/structure_table.sql
@@ -1,10 +1,7 @@
---
--- Table structure for table `data_file`
---
+USE epsi;
 
 DROP TABLE IF EXISTS `data_file`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `data_file` (
   `id` int NOT NULL AUTO_INCREMENT,
   `file_id` int DEFAULT NULL,
@@ -14,18 +11,11 @@ CREATE TABLE `data_file` (
   KEY `IDX_37D0FDF293CB796C` (`file_id`),
   CONSTRAINT `FK_37D0FDF293CB796C` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `file`
---
 
 DROP TABLE IF EXISTS `file`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `file` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
